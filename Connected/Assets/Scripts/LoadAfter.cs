9f3d1981669a2e26d96439a1c0d453b6f266a82e
SonicCane11 <51301700+SonicCane11@users.noreply.github.com>
2019-06-03 19:46:29 -0400

    Final....

---
+++ Connected/Assets/Scripts/LoadAfter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAfter : MonoBehaviour
+{
+    public string scene;
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waitLoad());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator waitLoad()
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(scene);
+    }
+}
