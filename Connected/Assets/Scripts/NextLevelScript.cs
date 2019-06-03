30966e80b09fc1488cd1fbccb88aab6a19fb4de7
SonicCane11 <51301700+SonicCane11@users.noreply.github.com>
2019-06-03 17:24:26 -0400

    load script working

--- Connected/Assets/Scripts/NextLevelScript.cs
+++ Connected/Assets/Scripts/NextLevelScript.cs
@@ -9,52 +9,47 @@ public class NextLevelScript : MonoBehaviour
     public bool redPlayerInside, bluePlayerInside;
     public NextLevelScript otherScript;
     public float time;
-    public float waitTime;
     public bool hasStarted = false;
+
+    void Update()
+    {
+
+        if (redPlayerInside && bluePlayerInside)
+        {
+            time -= Time.deltaTime;
+            if (time <= 0)
+            {
+                // sound
+                SceneManager.LoadScene(scene);
+            }
+        }
+    }
 
     void OnTriggerStay2D(Collider2D col)
     {
-        bluePlayerInside = redPlayerInside = false;
+        
         if (col.gameObject.CompareTag("BluePlayer"))
         {
-            Debug.Log("BluePlayer is at the end");
             bluePlayerInside = true;
         }
 
         if (col.gameObject.CompareTag("RedPlayer"))
         {
-            Debug.Log("RedPlayer is at the end");
             redPlayerInside = true;
         }
 
-        if (redPlayerInside && bluePlayerInside)
-        {
-            time -= Time.deltaTime;
-            if(time <=0 && !hasStarted)
-                SceneManager.LoadScene(scene);
-        }
     }
 
     void OnTriggerExit2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("BluePlayer"))
         {
-            Debug.Log("BluePlayer Left the end");
             bluePlayerInside = false;
         }
 
         if (col.gameObject.CompareTag("RedPlayer"))
         {
-            Debug.Log("RedPlayer Left the end");
             redPlayerInside = false;
         }
-    }
-
-    IEnumerator waitLoad()
-    {
-        hasStarted = true;
-        // play sound!
-        yield return new WaitForSeconds(waitTime);
-        SceneManager.LoadScene(scene);
     }
 }
